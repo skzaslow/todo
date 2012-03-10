319a4c13253b3bc69c8afb31b2489c74c393abc1
skzaslow <skzaslow@gmail.com>
2012-03-10 14:44:37 -0600

    changed tasks CSS and controller to sort by date

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.find(:all, :order => "due_date")
 
     respond_to do |format|
       format.html # index.html.erb
